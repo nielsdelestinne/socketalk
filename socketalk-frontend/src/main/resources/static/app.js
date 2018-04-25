740b8bd4a18536ec4417788b4919295ef39a0a49
nielsdelestinne <nielsdelestinne@gmail.com>
2018-04-25 23:16:11 +0200

    extended frontend

--- socketalk-frontend/src/main/resources/static/app.js
+++ socketalk-frontend/src/main/resources/static/app.js
@@ -1,15 +1,29 @@
 var stompClient = null;
 
 function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
+    if (!connected) {
+        $("#connect").show();
+        $("#name-for-connection").show();
+        $("#name-for-connection-label").show();
+        $("#conversation").hide();
+        $("#connections-overview").hide();
+        $("#disconnect").hide();
+        $("#send").hide();
+        $("#message").hide();
+        $("#message-label").hide();
     }
     else {
-        $("#conversation").hide();
+        $("#connect").hide();
+        $("#name-for-connection").hide();
+        $("#name-for-connection-label").hide();
+        $("#conversation").show();
+        $("#connections-overview").show();
+        $("#disconnect").show();
+        $("#send").show();
+        $("#message").show();
+        $("#message-label").show();
     }
-    $("#greetings").html("");
+    $("#all-messages").html("");
 }
 
 function connect() {
@@ -18,9 +32,9 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
-        });
+        subsribeToInitialConnectionTopic();
+        subsribeToAllMessagesTopic();
+        sendInitialConnectMessage();
     });
 }
 
@@ -32,19 +46,40 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+function sendInitialConnectMessage() {
+    stompClient.send("/app/initial-connection", {}, JSON.stringify({'name': $("#name-for-connection").val()}));
 }
 
-function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+function sendMessage() {
+    stompClient.send("/app/message", {}, JSON.stringify({'text': $("#message").val()}));
+}
+
+function subsribeToInitialConnectionTopic() {
+    stompClient.subscribe('/topic/initial-connection-greeting', function (greeting) {
+        showConnections(JSON.parse(greeting.body).content);
+    });
+}
+
+function subsribeToAllMessagesTopic() {
+    stompClient.subscribe('/topic/all-messages', function (message) {
+        showMessages(JSON.parse(message.body).text);
+    });
+}
+
+function showMessages(message) {
+    $("#all-messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+function showConnections(connection) {
+    $("#connections").append("<tr><td>" + connection + "</td></tr>");
 }
 
 $(function () {
+    setConnected(false);
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    $( "#send" ).click(function() { sendMessage(); });
 });
