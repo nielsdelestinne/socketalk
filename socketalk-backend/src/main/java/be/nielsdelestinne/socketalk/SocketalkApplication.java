fe30ef41a4f8454fbfa00203fce50d26643084e8
nielsdelestinne <nielsdelestinne@gmail.com>
2018-04-25 20:26:28 +0200

    initial commit containing basic gradle multi-module setup with initial dependencies configured

---
+++ socketalk-backend/src/main/java/be/nielsdelestinne/socketalk/SocketalkApplication.java
@@ -0,0 +1,12 @@
+package be.nielsdelestinne.socketalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketalkApplication.class, args);
+	}
+}
